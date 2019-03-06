d22b63d0dac088637095b13b801407c51d131c25
BorsiPatrick <48277523+BorsiPatrick@users.noreply.github.com>
2019-03-06 12:27:13 +0100

    Update 0mag.cpp

--- Hi turing/0mag.cpp
+++ Hi turing/0mag.cpp
@@ -1,3 +1,11 @@
+/*
+
+sleep() fugveny standby-ba rakja a programot,
+mivel ez egy vegtelen ciklusban van meghivva, 
+ezert a program vegig sleep-elni fog,ezert 0%-ban terheli a gepet
+
+*/
+
 #include <unistd.h>
 #include <stdio.h>
 using namespace std;
