04a57a9e7eae1470fe91a1aed0ffe98e626da279
BorsiPatrick <48277523+BorsiPatrick@users.noreply.github.com>
2019-03-06 12:29:35 +0100

    Update osszmag.cpp

--- Hi turing/osszmag.cpp
+++ Hi turing/osszmag.cpp
@@ -1,3 +1,7 @@
+/*
+#pragma omp tobb szalra ossza fel a for ciklust, tehat tobb magon fog futni
+a for vegtelen, ezert mindne magot leterhel
+*/
 #include <unistd.h>
 #include <omp.h>
 using namespace std;
